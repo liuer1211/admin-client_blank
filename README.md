07d7b27bea7528aa467555c8563387b9497216b8
wdd <296151852@qq.com>
2021-07-08 21:13:51 +0800

    1

---
+++ README.md
@@ -0,0 +1,11 @@
+## 介绍
+    react 管理台项目
+
+## 基本操作
+    yarn install
+    yarn start
+    yarn run build
+
+
+
+
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// 将App组件标签 渲染到index页面的div上
+ReactDOM.render(<App />, document.getElementById('root'));
+
