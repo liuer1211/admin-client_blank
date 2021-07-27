5dfafbaccc41f71e2906214ed74a5ce2cad6f587
wdd <296151852@qq.com>
2021-07-27 21:27:07 +0800

    1

--- src/components/left-nav/index.jsx
+++ src/components/left-nav/index.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import { Link } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 import { Menu, Icon } from 'antd';
 
 import menuList from '../../config/menuConfig';
@@ -9,13 +9,13 @@ import './index.less'
 
 const { SubMenu } = Menu;
 
-export default class LeftNav extends Component{
+class LeftNav extends Component{
 
     /*
         根据menu的数据数组生成对应的标签数组 
         map + 递归调用
     */
-    getMenuNodes = (menuList) => {
+    getMenuNodes_map = (menuList) => {
         return menuList.map(item=>{
             // 单个菜单
             if (!item.children) {
@@ -39,7 +39,7 @@ export default class LeftNav extends Component{
                         }
                     >
                         {/* 递归 */}
-                        { this.getMenuNodes(item.children) }
+                        { this.getMenuNodes_map(item.children) }
                     </SubMenu>
                 )
             }
@@ -50,12 +50,42 @@ export default class LeftNav extends Component{
         根据menu的数据数组生成对应的标签数组 
         reduce + 递归调用
     */
-    getMenuNodes_map = (menuList) => {
-        return ;
+    getMenuNodes = (menuList) => {
+        return menuList.reduce((pre,item)=>{
+            // pre添加<menu.Item>
+            if (!item.children) {
+                pre.push(
+                    (<Menu.Item key={item.key}>
+                        <Link to={item.key}>
+                            <Icon type={item.icon} />
+                            <span>{item.title}</span>
+                        </Link>
+                    </Menu.Item>)
+                )
+            } else { // pre添加<SubMenu>
+                pre.push(
+                    (<SubMenu
+                        key={item.key}
+                        title={
+                            <span>
+                                <Icon type={item.icon} />
+                                <span>{item.title}</span>
+                            </span>
+                        }
+                    >
+                        {/* 递归 */}
+                        { this.getMenuNodes(item.children) }
+                    </SubMenu>)
+                )
+            }
+            return pre;
+        },[])
     }
 
 
     render() {
+        // 得到当前路径
+        const path = this.props.location.pathname
         return (
             <div className="left-nav">
                 <Link to='/' className="left-nav-header">
@@ -63,7 +93,8 @@ export default class LeftNav extends Component{
                     <h1>管理系统</h1>
                 </Link>
                 <Menu
-                    defaultSelectedKeys={['1']}
+                    selectedKeys={[path]}
+                    defaultOpenKeys={['']}
                     mode="inline"
                     theme="dark"
                 >
@@ -105,3 +136,7 @@ export default class LeftNav extends Component{
         )
     }
 }
+// withRouter,高阶组件：
+// 包装非路由组件，返回一个新的组件
+// 心得组件向非路由组件传递三个属性：history、location、match
+export default withRouter(LeftNav)
