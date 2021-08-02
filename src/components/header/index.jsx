e888c2fa9d65a45888bee55dff742e7449171565
wdd <296151852@qq.com>
2021-08-02 21:32:33 +0800

    1

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,13 +1,17 @@
 import React, {Component} from 'react'
+import { withRouter } from 'react-router-dom'
+import { Modal } from 'antd';
 
 import {formateDate} from '../../utils/dateUtils'
 import memoryUtils from "../../utils/memoryUtils";
+import storageUtils from "../../utils/storageUtils";
+import menuList from "../../config/menuConfig"
 
 import {reqWeather} from "../../api/index"
 
 import './index.less'
 
-export default class Header extends Component{
+class Header extends Component{
 
     state = {
         currentTime: formateDate(Date.now()),
@@ -17,7 +21,7 @@ export default class Header extends Component{
 
     // 更新时间
     getTime = () => {
-        setInterval(()=>{
+        this.interId = setInterval(()=>{
             const currentTime = formateDate(Date.now());
             this.setState({currentTime})
         },1000)
@@ -28,6 +32,34 @@ export default class Header extends Component{
         // 获得接口数据
        const {dayPictureUrl, weather} = await reqWeather('上海')
        this.setState({dayPictureUrl, weather})
+    }
+
+    // 获得title
+    getTitle = ()=> {
+        const path = this.props.location.pathname
+        let title = ''
+        menuList.forEach(item=>{
+            if (item.key === path) {
+                title = item.title;
+            } else if (item.children) {
+                const it = item.children.find(it => it.key===path)
+                if (it) {
+                    title = it.title;
+                }
+            }
+        })
+        return title;
+    }
+
+    outLogin= () => {
+        Modal.confirm({
+            title: '确定退出吗？',
+            onOk: ()=>{
+                storageUtils.removeUser()
+                memoryUtils.user={}
+                this.props.history.replace('/login')
+            }
+        })
     }
     
 
@@ -40,20 +72,28 @@ export default class Header extends Component{
         this.getWeather();
     }
 
+    // 当前组件卸载前执行
+    componentWillUnmount() {
+        // 清除定时器
+        clearInterval(this.interId)
+    }
+
     render() {
 
         const {currentTime, dayPictureUrl, weather} =this.state
 
         const user = memoryUtils.user.username
 
+        const title = this.getTitle(); // 要在这里获取，切换时会执行这里，不执行其他钩子函数，不能写在state里
+
         return (
             <div className="header">
                 <div className="header-top">
                     <span>欢迎，{user}</span>
-                    {/* <a href="#">退出</a> */}
+                    <span onClick={this.outLogin}>退出</span>
                 </div>
                 <div className="header-bot">
-                    <div className="header-bot-l">首页</div>
+                    <div className="header-bot-l">{title}</div>
                     <div className="header-bot-r">
                         <span className="header-bot-r-sp1">{currentTime}</span>
                         <img src={dayPictureUrl} alt="天气" />
@@ -64,3 +104,5 @@ export default class Header extends Component{
         )
     }
 }
+
+export default withRouter(Header)
