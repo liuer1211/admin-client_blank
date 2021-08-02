c1cab1f263c587ad8bb93b0ecd37d0b00e486ea5
wdd <296151852@qq.com>
2021-08-02 21:33:41 +0800

    1

---
+++ build/precache-manifest.d1f6354c4a13c5453731345b5b432d4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f23e111f11b5f5438a4be022fd816bc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6082e00b87aacac1f92f",
+    "url": "./static/css/2.56580aea.chunk.css"
+  },
+  {
+    "revision": "8af39ff1e8424dd9d7e8",
+    "url": "./static/css/main.4a3de1aa.chunk.css"
+  },
+  {
+    "revision": "6082e00b87aacac1f92f",
+    "url": "./static/js/2.0c431982.chunk.js"
+  },
+  {
+    "revision": "8af39ff1e8424dd9d7e8",
+    "url": "./static/js/main.28dfd824.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "0ce0069ee5fecb17e3f0a9ed5cafd8f7",
+    "url": "./static/media/logo.0ce0069e.png"
+  }
+]);
