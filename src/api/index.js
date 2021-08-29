4649b310f7e6f53dea469bd4fdce73e057048178
wdd <296151852@qq.com>
2021-08-29 21:55:05 +0800

    1

--- src/api/index.js
+++ src/api/index.js
@@ -22,8 +22,23 @@ export const reqAddCategory= (categoryName, parentId) => ajax(BASE + '/manage/ca
 // 修改商品分类
 export const reqUpdateCategory= ({categoryName, categoryId}) => ajax(BASE + '/manage/category/update', {categoryName, categoryId}, 'post');
 
+/*
+搜索商品分页列表 (根据商品名称/商品描述)
+searchType: 搜索的类型, productName/productDesc
+ */
+export const reqSearchProducts = ({pageNum, pageSize, searchName, searchType}) => ajax(BASE + '/manage/product/search', {
+    pageNum,
+    pageSize,
+    [searchType]: searchName,
+  })
+// 获取商品分页列表
+export const reqProducts = (pageNum, pageSize) => ajax(BASE + '/manage/product/list', {pageNum, pageSize})
 
+// 更新商品的状态(上架/下架)
+export const reqUpdateStatus = (productId, status) => ajax(BASE + '/manage/product/updateStatus', {productId, status}, 'POST')
 
+// 添加/修改商品
+export const reqAddOrUpdateProduct = (product) => ajax(BASE + '/manage/product/' + ( product._id?'update':'add'), product, 'POST')
 
 
 
---
+++ src/pages/product/add-update.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class ProductAddUpdate extends React.Component{
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default ProductAddUpdate;
